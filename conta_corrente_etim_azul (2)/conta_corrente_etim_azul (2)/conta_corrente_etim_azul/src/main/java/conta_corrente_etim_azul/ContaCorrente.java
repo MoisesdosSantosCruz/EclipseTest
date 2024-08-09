ef9ec6ac279c6563c46d6d7686fa71e19d15bc2f
3DS <3ds@bglab.local>
2024-08-09 11:57:37 -0300

    Mandando ao Git

---
+++ conta_corrente_etim_azul (2)/conta_corrente_etim_azul (2)/conta_corrente_etim_azul/src/main/java/conta_corrente_etim_azul/ContaCorrente.java
@@ -0,0 +1,49 @@
+package conta_corrente_etim_azul;
+
+
+public class ContaCorrente {
+	public String nomeTitular;
+	public double saldo;
+	
+	
+	public void depositar(double valor) {
+		this.saldo += valor;
+	}
+
+	ContaCorrente(String nomeTitular, double saldo) {
+		this.nomeTitular = nomeTitular;
+		this.saldo = saldo;
+	}
+	
+	ContaCorrente(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+		
+	}
+	
+	ContaCorrente(double saldo) {
+		this.saldo = saldo;
+		
+	}
+	public boolean sacar(double valor) {
+		if(this.saldo < valor) {
+			return false;
+		}
+		this.saldo -= valor;
+		return true;
+	}
+	
+	public boolean tranferir(double valor, ContaCorrente contaDestino) {
+		if(this.saldo < valor) {
+			return false;
+		}
+		this.saldo -= valor;
+		contaDestino.saldo += valor;
+		return true;
+		
+		
+		
+		
+		
+	}
+
+}
